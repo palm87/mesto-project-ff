aca2f01a3e3ac8bf2c6144e30990335656280925
Ksenia Semenova <palm87@gmail.com>
2024-02-23 12:39:04 +0300

    Работа над 6 проектом, сделано редактирование профиля

--- scripts/index.js
+++ scripts/index.js
@@ -6,11 +6,118 @@ const cardTemplate = document.querySelector('#card-template').content;
 //определим область со списком карточек
 const cardsList=document.querySelector('.places__list');
 
+//-----------------------------------------ПОПАПЫ--------------------------------------------------
+//переменная для оверлея (чтобы в дальнейшем закрывать попап по клику на оверлей)
+const popup = document.querySelector('.popup')
+
+//переменная для кнопки закрытия попапа
+const popupCloseButton = document.querySelector(".popup__close")
+
+//закрытие попапа по клику на оверлей
+// popup.addEventListener('click', function(evt) {
+//     closePopup(evt.target)
+// })
+popup.addEventListener('click', function(evt) {
+    closePopup(popup)
+})
+
+
+
+// функция для открытия попапов
+function openPopup(popupElement) {
+    popupElement.classList.add('popup_is-opened');
+    document.addEventListener('keydown', escClosePopup);
+
+  }
+ 
+ //функция для закрытия попапов 
+function closePopup(popupElement) {
+    popupElement.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', escClosePopup);
+    
+  }
+//закрытие попапа редактирования профиля по клику на крестик
+popupCloseButton.addEventListener('click', function(evt) {
+    closePopup(popupEditProfile);
+    document.removeEventListener('keydown', escClosePopup);
+   
+})
+
+// функция закрытия попапа по нажатию на клавишу Esc
+function escClosePopup(evt) {
+    if (evt.key === 'Escape') {
+        closePopup(popup);
+    }
+}
+//-----------------------------------------РЕДАКТИРОВАНИЕ ПРОФИЛЯ--------------------------------------------------
+//переменная для формы редактирования профиля
+const formEditProfile = document.forms['edit-profile'];
+
+
+//переменные для заполнения значений по умолчанию в форме редактирования профиля
+//переменная для хранения имени профиля
+const profileName = document.querySelector('.profile__title');
+//заполняем инпут попапа для введения имени значением из профиля
+const profileNameInput = document.querySelector('.popup__input_type_name');
+profileNameInput.value=profileName.textContent;
+
+//переменная для хранения описания профиля
+const profileDescription = document.querySelector('.profile__description');
+//заполняем инпут попапа для введения описания значением из профиля
+const profileDescriptionInput = document.querySelector('.popup__input_type_description');
+profileDescriptionInput.value=profileDescription.textContent;
+
+//переменная для кнопки редактирования профиля
+const editProfileButton = document.querySelector('.profile__edit-button');
+
+//слушатель для открытия попапа редактирования профиля
+editProfileButton.addEventListener('click', function () {
+    openPopup(popupEditProfile);
+    document.addEventListener('keydown', escClosePopup);
+})
+
+
+//переменная для попапа с редактированием профиля
+const popupEditProfile = document.querySelector('.popup_type_edit');
+
+// функция редактирование профиля
+function handleFormSubmit(evt) {
+    evt.preventDefault(); 
+    profileName.textContent = profileNameInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup(popupEditProfile)
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formEditProfile.addEventListener('submit', handleFormSubmit);
+
+
+//-----------------------------------------ДОБАВЛЕНИЕ НОВОЙ КАРТОЧКИ--------------------------------------------------
+//переменная формы для добавления новой карточки
+const formAddNewPlace = document.forms['new-place'];
+
+//переменная для попапа добавления новой карточки
+const popupAddNewCard = document.querySelector('.popup_type_new-card')
+
+
+//переменная для кнопки добавления карточки
+const addNewCardButton = document.querySelector('.profile__add-button');
+
+//слушатель для открытия попапа редактирования профиля
+addNewCardButton.addEventListener('click', function () {
+    openPopup(popupAddNewCard);
+    document.addEventListener('keydown', escClosePopup);
+})
+
+
+
+
+
+
+
 //для показа надписи, когда нет ни одной карточки
 const zeroPlaces = document.querySelector('.places-zero');
-
-//кнопка для перезагрузки списка карточек
-const reloadButton = document.querySelector('.profile__reload-button');
 
 //добавим счетчик карточек, чтобы отслеживать, когда нет ни одной карточки
 let cardsCount=0;
@@ -34,13 +141,7 @@ function createCard(card, deleteCard) {
     //добавим eventListner на клик по кнопке удаления
     cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
         deleteCard(cardElement);
-    });
-
-    //вариант с передачей Event
-    // cardElement.querySelector('.card__delete-button').addEventListener('click', function (event){
-    //     deleteOnEvent(event);
-    // });
-    
+    });    
     return cardElement;
 }
 
@@ -51,11 +152,6 @@ function deleteCard(card) {
     cardsCount-=1;
     checkCardsCount();
 }
-
-//функция удаления в случае передаи Event
-// function deleteOnEvent(event) {
-//     event.target.closest('.card').remove();
-// }
 
 // @todo: Вывести карточки на страницу
 //функция добавления карточки в отображенный список на странице
@@ -71,31 +167,8 @@ initialCards.forEach((item) => {
 
 //--------------------------EXTRA--------------------------------------------
 
-//функция для обновления списка карточек - удаляет все загруженные и заново подргужает список из initialCards
-function reloadCardList() {
-    const allCards=document.querySelectorAll('.card')
-    allCards.forEach((item) =>{
-        item.remove()
-    })
-    cardsCount=0;
-    initialCards.forEach((item) => {
-        renderCard(createCard(item, deleteCard));
-    });
-    checkCardsCount();
-    checkReloadButton();
-}
-
-//добавим обновление списка карточек по отдельно созданной кнопке 
-reloadButton.addEventListener('click', reloadCardList);
-
 //функция для проверки, сколько карточек отображается и не пора ли отобразить надпись, что карточек нет
 function checkCardsCount () {
     zeroPlaces.classList.toggle('hide-it', cardsCount !== 0)
-    checkReloadButton();
-}
-
-//если на данный момент все карточки из initialCards отображены - дизейблим кнопку
-function checkReloadButton() {
-    reloadButton.disabled = cardsCount===initialCards.length;
 }
 
